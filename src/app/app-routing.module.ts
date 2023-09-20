3168a5c06b661f16a335e6882c7651f91df0e456
MarcosAdaSilva <marcaofly@gmail.com>
2023-09-19 23:57:44 -0300

    Rota para LOGIN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-list.component";
-import { HomeComponent } from "./components/home/home.component";
-import { NavComponent } from "./components/nav/nav.component";
+import { LoginComponent } from "./components/login/login.component";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+import { HomeComponent } from "./components/home/home.component";
+import { NavComponent } from "./components/nav/nav.component";
+import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-list.component";
+
 const routes: Routes = [
+  { path: "login", component: LoginComponent },
   {
     path: "",
     component: NavComponent,
